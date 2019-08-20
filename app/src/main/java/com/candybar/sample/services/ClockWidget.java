9401ba6a9bfd085abe679a2a820ab51dee027329
sarsamurmu <sarsamurmu05@gmail.com>
2019-08-20 15:33:22 +0530

    Prepare for v3.5.0-b6

---
+++ app/src/main/java/com/candybar/sample/services/ClockWidget.java
@@ -0,0 +1,5 @@
+package com.candybar.sample.services;
+
+import candybar.lib.services.CandyBarWidgetService;
+
+public class ClockWidget extends CandyBarWidgetService{}
--- app/src/main/java/com/candybar/sample/services/MuzeiService.java
+++ app/src/main/java/com/candybar/sample/services/MuzeiService.java
@@ -2,11 +2,4 @@ package com.candybar.sample.services;
 
 import candybar.lib.services.CandyBarMuzeiService;
 
-public class MuzeiService extends CandyBarMuzeiService {
-
-    private static final String SOURCE_NAME = "CandyBar:MuzeiArtSource";
-
-    public MuzeiService() {
-        super(SOURCE_NAME);
-    }
-}
+public class MuzeiService extends CandyBarMuzeiService {}
