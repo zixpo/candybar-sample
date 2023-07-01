d90e27a2a336fecc61895e25d6a7555e93caf805
Sarsa Murmu <sarsamurmu05@gmail.com>
2023-07-01 20:26:08 +0530

    Update for v3.18.0

--- app/src/main/java/com/candybar/sample/applications/CandyBar.java
+++ app/src/main/java/com/candybar/sample/applications/CandyBar.java
@@ -5,6 +5,8 @@ import androidx.annotation.NonNull;
 // TODO: Remove `//` below to enable OneSignal
 //import com.onesignal.OneSignal;
 
+import com.candybar.sample.R;
+
 import candybar.lib.applications.CandyBarApplication;
 
 public class CandyBar extends CandyBarApplication {
@@ -20,6 +22,12 @@ public class CandyBar extends CandyBarApplication {
         OneSignal.setAppId("YOUR_ONESIGNAL_APP_ID_HERE");
     }
     */
+
+    @NonNull
+    @Override
+    public Class<?> getDrawableClass() {
+        return R.drawable.class;
+    }
 
     @NonNull
     @Override
